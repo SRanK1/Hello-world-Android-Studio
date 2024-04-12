11795ed29d17a025e26423ecd1b4ec8704399201
SRanK1 <kevin.alfaro@unmsm.edu.pe>
2024-04-12 00:37:58 -0500

    Commit inicial

---
+++ app/src/main/java/com/example/helloworld/Variables.kt
@@ -0,0 +1,9 @@
+package com.example.helloworld
+
+fun main() {
+    val numberOfPhotos = 100
+    val photosDeleted = 10
+    println("$numberOfPhotos photos")
+    println("$photosDeleted photos deleted")
+    println("${numberOfPhotos - photosDeleted} photos left")
+}
